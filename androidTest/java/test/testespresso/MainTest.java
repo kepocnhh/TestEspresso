556bb0bdabcd0c9571231708d0005d99339a8470
antonoctopod <a.grechushnikov@octopod.com>
2016-02-26 13:45:31 +0300

    find bug with russian letters

--- androidTest/java/test/testespresso/MainTest.java
+++ androidTest/java/test/testespresso/MainTest.java
@@ -1,5 +1,9 @@
 package test.testespresso;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -10,19 +14,11 @@ import org.junit.runner.RunWith;
 
 import test.testespresso.ui.activities.Main;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainTest
 {
-    public static final String STRING_TO_BE_TYPED = "Espresso";
+    public static final String STRING_TO_BE_TYPED = "Espresso test &&&& ........................ android ui test БЕЗНОГNМ";
 
     @Rule
     public ActivityTestRule<Main> mActivityRule = new ActivityTestRule<>(Main.class);
@@ -30,12 +26,12 @@ public class MainTest
     @Test
     public void changeText_sameActivity()
     {
-        onView(withId(R.id.main_test_edit))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
-        onView(withId(R.id.main_test_button))
-                .perform(click());
-        onView(withId(R.id.main_test_text))
-                .check(matches(withText(STRING_TO_BE_TYPED)));
+        Espresso.onView(ViewMatchers.withId(R.id.main_test_edit))
+                .perform(ViewActions.typeText(STRING_TO_BE_TYPED), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.main_test_button))
+                .perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.main_test_text))
+                .check(ViewAssertions.matches(ViewMatchers.withText(STRING_TO_BE_TYPED)));
     }
 
 //    @Test
